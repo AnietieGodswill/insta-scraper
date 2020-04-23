8d96c1d79dc49fb8ce35b48231af0d6ceacbc1e7
Nishant Tiwari <dx4iot@gmail.com>
2020-04-23 15:45:09 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="Insta Scraper",
+version="0.1",
+description="Insta Scraper",
+author="Nishant Tiwari",
+url="https://github.com/dx4iot/insta-scraper",
+install_requires=[
+    'requests>=2.21.0',
+    'bs4>=4.8.2']
+)
